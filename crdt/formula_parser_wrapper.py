fe8436a30a909ae8ff041e491a062bff1fc5e98c
Fabian Gubler <fabian.gubler@hotmail.com>
2023-11-21 02:19:49 +0100

    add is equal function to wrapper

--- crdt/formula_parser_wrapper.py
+++ crdt/formula_parser_wrapper.py
@@ -1,10 +1,15 @@
 from parser.formula_parser import parse, FormulaParseError
-
+from datetime import datetime
+from crdt.custom_ast_node import CustomASTNode
 
 class FormulaParserWrapper:
-    def parse_formula(self, formula):
+    def parse_formula(self, formula, user_id=None):
         try:
-            return parse(formula)
+            ast_node = parse(formula)
+            return CustomASTNode(ast_node, user_id)
         except FormulaParseError as error:
-            # Handle the exception or re-raise with additional context
             raise Exception(f"Error parsing formula: {error}")
+
+    def reconstruct_formula(self, ast):
+        # Recursive function to convert AST back to formula string
+        pass
