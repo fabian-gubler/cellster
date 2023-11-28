c33905740f0ad2859d9e533ac0206063cf1b938a
Fabian Gubler <fabian.gubler@hotmail.com>
2023-11-28 12:06:27 +0100

    add test cases for ast parsing

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,24 +1,95 @@
-# test_parser.py
 import pytest
-from parser.formula_parser import parse, FormulaParseError
+from parser.parser import parse, FormulaParseError
+from parser.ast_nodes import (
+    Cell,
+    CellRange,
+    Name,
+    Function,
+    Number,
+    Logical,
+    Binary,
+    Unary,
+)
 
 
-def test_parse_valid_formula():
-    # Test parsing a valid formula
-    formula1 = "SUM(A1:A10)"
+def test_parse_number():
+    ast = parse("123")
+    assert isinstance(ast, Number)
+    assert ast.value == 123
+    assert ast.user_id == "default_user"
 
-    # Test parsing a complex formula
-    formula2 = "HELLO(SUM(A1:A3), 1+2*3-ABS(B14-MY_FUNC(C67)), IF(true, false, -10e-5))"
-    ast1 = parse(formula1)
-    ast2 = parse(formula2)
-
-    # TODO: Validate the structure of the AST
-    assert ast1 is not None  # Basic check
-    assert ast2 is not None  # Complex check
+    ast = parse("3.14")
+    assert isinstance(ast, Number)
+    assert ast.value == 3.14
 
 
-def test_parse_invalid_formula():
-    # Test parsing an invalid formula
-    formula = "=SUM("
+def test_parse_logical():
+    ast = parse("TRUE")
+    assert isinstance(ast, Logical)
+    assert ast.value is True
+
+    ast = parse("FALSE")
+    assert isinstance(ast, Logical)
+    assert ast.value is False
+
+
+def test_parse_cell():
+    ast = parse("A1")
+    assert isinstance(ast, Cell)
+    assert ast.col == "A"
+    assert ast.row == 1
+
+
+def test_parse_cell_range():
+    ast = parse("A1:B2")
+    assert isinstance(ast, CellRange)
+    assert isinstance(ast.start, Cell)
+    assert isinstance(ast.end, Cell)
+    assert ast.start.col == "A"
+    assert ast.start.row == 1
+    assert ast.end.col == "B"
+    assert ast.end.row == 2
+
+
+def test_parse_function():
+    ast = parse("SUM(A1, B2)")
+    assert isinstance(ast, Function)
+    assert ast.func_name == "SUM"
+    assert len(ast.arguments) == 2
+    assert isinstance(ast.arguments[0], Cell)
+    assert isinstance(ast.arguments[1], Cell)
+
+
+def test_parse_binary_operation():
+    ast = parse("1 + 2")
+    assert isinstance(ast, Binary)
+    assert ast.op == "+"
+    assert isinstance(ast.left, Number)
+    assert isinstance(ast.right, Number)
+    assert ast.left.value == 1
+    assert ast.right.value == 2
+
+
+def test_parse_unary_operation():
+    ast = parse("-1")
+    assert isinstance(ast, Unary)
+    assert ast.op == "-"
+    assert isinstance(ast.expr, Number)
+    assert ast.expr.value == 1
+
+
+def test_parse_complex_expression():
+    ast = parse("SUM(A1:B2) * 3")
+    assert isinstance(ast, Binary)
+    assert ast.op == "*"
+    assert isinstance(ast.left, Function)
+    assert isinstance(ast.right, Number)
+
+
+def test_parse_error_incomplete_function_call():
     with pytest.raises(FormulaParseError):
-        parse(formula)
+        parse("SUM(")  # Incomplete function call
+
+def test_parse_error_incomplete_expression():
+    with pytest.raises(FormulaParseError):
+        parse("A1 + ")  # Incomplete expression
