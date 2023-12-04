afbf13ffcb287b7363f0addbae1376e7bca68e82
Fabian Gubler <fabian.gubler@protonmail.com>
2023-12-04 22:51:58 +0100

    formatting with black

--- tests/test_apply_changes.py
+++ tests/test_apply_changes.py
@@ -53,6 +53,7 @@ def test_nested_modification():
 # Addition / Deletion #
 #######################
 
+
 def test_apply_additions():
     original_ast = parse("SUM(A1:A10)")
     modified_ast = parse("SUM(A1:A10, 5)")
@@ -73,6 +74,7 @@ def test_apply_deletions():
 # ROOT LEVEL
 ######################
 
+
 def test_apply_root_additions():
     original_ast = parse("SUM(A1:A10)")
     modified_ast = parse("SUM(A1:A10) + 1")
@@ -82,6 +84,7 @@ def test_apply_root_additions():
         new_ast = apply_changes_to_ast(original_ast, changes)
 
     # assert str(new_ast) == "SUM(A1:A10 + 1)"
+
 
 def test_apply_root_deletions():
     original_ast = parse("SUM(A1:A10) + 1")
