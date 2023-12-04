9587002741c15beeaefc4d24b2fcb648db6a8c69
Fabian Gubler <fabian.gubler@protonmail.com>
2023-12-04 23:26:38 +0100

    add many complex test cases to applying changes

--- tests/test_apply_changes.py
+++ tests/test_apply_changes.py
@@ -33,20 +33,49 @@ def test_nested_modification():
     assert str(new_ast) == "SUM(AVERAGE(A1:A6), A10)"
 
 
-# def test_apply_structural_changes():
-#     original_ast = parse("SUM(A1:A10)")
-#     modified_ast = parse("AVERAGE(A1:A10)")
-#     changes = compare_asts(original_ast, modified_ast)
-#     new_ast = apply_changes_to_ast(original_ast, changes)
-#     assert str(new_ast) == "AVERAGE(A1:A10)"
-#
-#
-# def test_nested_structural_changes():
-#     original_ast = "SUM(AVERAGE(A1:A5), A10)"
-#     modified_ast = "AVERAGE(SUM(A1:A5), A10)"
-#     changes = compare_asts(original_ast, modified_ast)
-#     new_ast = apply_changes_to_ast(original_ast, changes)
-#     assert str(new_ast) == "AVERAGE(SUM(A1:A5), A10)"
+def test_apply_structural_changes():
+    original_ast = parse("SUM(A1:A10)")
+    modified_ast = parse("AVERAGE(A1:A10)")
+    changes = compare_asts(original_ast, modified_ast)
+    new_ast = apply_changes_to_ast(original_ast, changes)
+    assert str(new_ast) == "AVERAGE(A1:A10)"
+
+
+#########################
+# COMPLEX MODIFICATIONS #
+#########################
+
+
+def test_nested_composite_modifications():
+    original_ast = parse("SUM(AVERAGE(A1:A5), A10)")
+    modified_ast = parse("SUM(AVERAGE(A2:A6), A11)")
+    changes = compare_asts(original_ast, modified_ast)
+    new_ast = apply_changes_to_ast(original_ast, changes)
+    assert str(new_ast) == "SUM(AVERAGE(A2:A6), A11)"
+
+
+def test_nested_structural_changes():
+    original_ast = parse("SUM(AVERAGE(A1:A5), A10)")
+    modified_ast = parse("AVERAGE(SUM(A1:A5), A10)")
+    changes = compare_asts(original_ast, modified_ast)
+    new_ast = apply_changes_to_ast(original_ast, changes)
+    assert str(new_ast) == "AVERAGE(SUM(A1:A5), A10)"
+
+
+def test_mixed_modifications_and_structural_changes():
+    original_ast = parse("SUM(A1:A10, AVERAGE(B1:B5))")
+    modified_ast = parse("SUM(A2:A9, MAX(B2:B6))")
+    changes = compare_asts(original_ast, modified_ast)
+    new_ast = apply_changes_to_ast(original_ast, changes)
+    assert str(new_ast) == "SUM(A2:A9, MAX(B2:B6))"
+
+
+def test_root_level_composite_changes():
+    original_ast = parse("SUM(A1:A10, B1:B10)")
+    modified_ast = parse("SUM(A2:A9, B2:B9)")
+    changes = compare_asts(original_ast, modified_ast)
+    new_ast = apply_changes_to_ast(original_ast, changes)
+    assert str(new_ast) == "SUM(A2:A9, B2:B9)"
 
 
 #######################
@@ -68,6 +97,15 @@ def test_apply_deletions():
     changes = compare_asts(original_ast, modified_ast)
     new_ast = apply_changes_to_ast(original_ast, changes)
     assert str(new_ast) == "SUM(A1:A10)"
+
+
+def test_complex_structural_change():
+    original_ast = parse("SUM(A1:A10)")
+    modified_ast = parse("A1 + A2 + A3 + A4 + A5 + A6 + A7 + A8 + A9 + A10")
+    changes = compare_asts(original_ast, modified_ast)
+    with pytest.raises(StructuralChangeException):
+        new_ast = apply_changes_to_ast(original_ast, changes)
+    # assert str(new_ast) == "A1 + A2 + A3 + A4 + A5 + A6 + A7 + A8 + A9 + A10"
 
 
 ######################
