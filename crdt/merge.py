5e4c4806f3b124e43f3e21e0dd2b727aa451554b
Fabian Gubler <fabian.gubler@protonmail.com>
2023-12-05 02:44:07 +0100

    change input from node list to change dictionaries

--- crdt/merge.py
+++ crdt/merge.py
@@ -3,7 +3,7 @@ from parser.tree_operations import find_node, replace_node
 class NodeNotFoundError(Exception):
     pass
 
-def merge_ast(original_ast, updated_nodes):
+def merge_ast(original_ast, changes):
     def calculate_depth(original_history, updated_history):
         min_length = min(len(original_history), len(updated_history))
         for i in range(min_length):
@@ -11,16 +11,20 @@ def merge_ast(original_ast, updated_nodes):
                 return len(updated_history) - i - 1
         return -1 if len(original_history) > len(updated_history) else 0
 
-    for updated_node in updated_nodes:
-        original_node = find_node(original_ast, updated_node.id_history)
-        if not original_node:
-            raise NodeNotFoundError("Node not found in original AST")
-        depth = calculate_depth(original_node.id_history, updated_node.id_history)
+    for change in changes:
+        if change['type'] == 'modification':
+            modified_node = change['modified']
+            original_node = find_node(original_ast, modified_node.id_history)
+            if not original_node:
+                raise NodeNotFoundError("Node not found in original AST")
+            depth = calculate_depth(original_node.id_history, modified_node.id_history)
 
-        if depth > 0:
-            replace_node(original_ast, original_node.id_history, updated_node)
-        elif depth == 0:
-            conflict_resolution(original_node, updated_node)  # Placeholder for conflict resolution logic
+            if depth > 0:
+                replace_node(original_ast, original_node.id_history, modified_node)
+            elif depth == 0:
+                conflict_resolution(original_node, modified_node)  # Placeholder for conflict resolution logic
+
+        # Additional handling for 'addition' and 'deletion' types if necessary
 
     return original_ast
 
