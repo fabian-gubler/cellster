a171b57c0d606a858d96ea5678e843e2d2e4d8cd
Fabian Gubler <fabian.gubler@protonmail.com>
2023-12-05 12:07:20 +0100

    refactor applying changes program

--- crdt/apply_changes.py
+++ crdt/apply_changes.py
@@ -1,15 +1,24 @@
 from parser.tree_operations import (
     delete_node,
     find_node,
-    replace_node,
     find_parent_and_child,
     add_node,
     delete_node,
 )
 
-from parser.ast_nodes import BaseNode, Function, Binary, Unary
+from parser.ast_nodes import (
+    Cell,
+    CellRange,
+    Name,
+    Function,
+    Number,
+    Logical,
+    Binary,
+    Unary,
+)
 
 from copy import deepcopy
+import uuid
 
 
 class StructuralChangeException(Exception):
@@ -19,50 +28,48 @@ class StructuralChangeException(Exception):
 def apply_changes_to_ast(original_ast, changes):
     updated_nodes = []
     for change in changes:
+        # print("Change detected:", change)  # Debugging statement
         if change["type"] == "modification":
-            modified_node = change["modification"]
-            original_node = find_node(original_ast, change["original"].id_history)
+            node_to_change = find_node(original_ast, change["original"].id_history)
 
-            if original_node is None:
+            if node_to_change is None:
                 raise StructuralChangeException("Node not found in original AST")
 
-            new_node = replace_node(
-                original_ast, original_node.id_history, change["modification"]
-            )
+            updated_node = replace_node(node_to_change, change["modification"])
 
-            # print("Original node ID history: ", original_node.id_history)
-            # print(
-            #     f"Applied change: {change["type"]}, Updated id_history: {change["modification"].id_history}"
-            # )
-
-            if new_node:
-                updated_nodes.append(new_node)
+            updated_nodes.append(updated_node)
 
         else:
             raise StructuralChangeException("Change type not found")
 
-        # elif change["type"] == "addition":
-        #     # Logic to add a node to the AST
-        #     # This uses add_node() from tree_operations.py
-        #     parent_node = find_node(original_ast, change["parent_id_history"])
-        #     if parent_node:
-        #         new_node = add_node(
-        #             parent_node, change["node"], child_side=change["child_side"]
-        #         )
-        #     else:
-        #         raise StructuralChangeException("Parent node not found")
-        #
-        #     if new_node:
-        #         updated_nodes.append(new_node)
-        #
-        # elif change["type"] == "deletion":
-        #     parent, child_to_delete = find_parent_and_child(
-        #         original_ast, change["node"].id_history
-        #     )
-        #     if parent and child_to_delete:
-        #         delete_node(parent, child_to_delete.id_history)
-        #     else:
-        #         raise StructuralChangeException("Parent node not found")
-
-
     return original_ast, updated_nodes
+
+def replace_node(node_to_change, changed_node):
+
+            if isinstance(node_to_change, Binary):
+                node_to_change.op = changed_node.op
+                node_to_change.generate_new_id()
+
+            elif isinstance(node_to_change, CellRange):
+                node_to_change.start = changed_node.start
+                node_to_change.end = changed_node.end
+                node_to_change.generate_new_id()
+
+            elif isinstance(node_to_change, Function):
+                node_to_change.func_name = changed_node.func_name
+                node_to_change.generate_new_id()
+
+            elif isinstance(node_to_change, Unary):
+                node_to_change.op = changed_node.op
+                node_to_change.generate_new_id()
+
+            elif isinstance(node_to_change, Cell):
+                node_to_change.col = changed_node.col
+                node_to_change.row = changed_node.row
+                node_to_change.generate_new_id()
+
+            else:
+                raise StructuralChangeException(type(node_to_change))
+
+            updated_node={"node": node_to_change, "type": "modification"}
+            return updated_node
