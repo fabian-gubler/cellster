a532e91e9678442c16106dcf94cc6ca9185136bd
Fabian Gubler <fabian.gubler@protonmail.com>
2023-12-05 12:06:08 +0100

    add rules to merge function

--- crdt/merge.py
+++ crdt/merge.py
@@ -1,4 +1,6 @@
-from parser.tree_operations import find_node, find_parent_and_child, replace_node, add_node, delete_node
+from parser.tree_operations import find_node, find_parent_and_child, add_node, delete_node
+from parser.ast_nodes import CellRange, Cell
+from crdt.apply_changes import replace_node
 
 class NodeNotFoundError(Exception):
     pass
@@ -29,10 +31,17 @@ def merge_ast(original_ast, changes):
 
             if not original_node:
                 raise NodeNotFoundError("Node not found in original AST")
+
+            # Enhanced conflict resolution for CellRange nodes
+            if isinstance(original_node, CellRange) and isinstance(modified_node, CellRange):
+                merged_range = merge_cell_ranges(original_node, modified_node)
+                replace_node(original_node, merged_range)
+
+
             depth = calculate_depth(original_node.id_history, modified_node.id_history)
 
             if depth > 0:
-                replace_node(original_ast, original_node.id_history, modified_node)
+                replace_node(original_node, modified_node)
             elif depth == 0:
                 conflict_resolution(original_node, modified_node)  # Placeholder for conflict resolution logic
 
@@ -52,7 +61,30 @@ def merge_ast(original_ast, changes):
 
     return original_ast
 
+
+def merge_cell_ranges(node1, node2):
+    print("Merging:", node1, "with", node2)  # Debug input CellRanges
+    start_row = min(node1.start.row, node2.start.row)
+    end_row = max(node1.end.row, node2.end.row)
+    start_col = min(node1.start.col, node2.start.col, key=lambda x: col_name_to_number(x))
+    end_col = max(node1.end.col, node2.end.col, key=lambda x: col_name_to_number(x))
+
+    # Creating the merged range
+    merged_range = CellRange(Cell(start_col, start_row, user_id="merged"), Cell(end_col, end_row, user_id="merged"), user_id="merged")
+
+    print("Merged range:", merged_range)  # Debug output
+
+    return merged_range
+
+
+def col_name_to_number(col):
+  """Converts an Excel column name to a number (e.g., 'A' -> 1, 'Z' -> 26, 'AA' -> 27)."""
+  number = 0
+  for char in col:
+      number = number * 26 + (ord(char.upper()) - ord('A') + 1)
+  return number
+
 def conflict_resolution(original_node, updated_node):
     # Placeholder function for conflict resolution
     # Implement conflict resolution logic here
-    pass
+    raise NotImplementedError
