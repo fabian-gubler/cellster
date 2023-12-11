1b5b9fe46e689b88e474e0fd449c98e8816868a1
Fabian Gubler <fabian.gubler@hotmail.com>
2023-12-11 13:56:47 +0100

    clean up dev environment

--- flake.nix
+++ flake.nix
@@ -13,11 +13,6 @@
         pythonPackages = pkgs.python310Packages;# <--- change here
         pkgs = import nixpkgs {
           inherit system;
-
-          # config = { # <--- uncomment for cuda support:
-          #   allowUnfree = true;
-          #   cudaSupport = true;
-          # };
         };
       in
       {
@@ -36,47 +31,12 @@
             # Dependencies that we would like to use from nixpkgs, which will
             # add them to PYTHONPATH and thus make them accessible from within the venv.
             pythonPackages.pytest
-
-            ## Developer tools
             pkgs.black
             pythonPackages.flake8
             pythonPackages.isort
-			pythonPackages.nbconvert
             pkgs.nodePackages.pyright
-
-
-            # In this particular example, in order to compile any binary extensions they may
-            # require, the Python modules listed in the hypothetical requirements.txt need
-            # the following packages to be installed locally:
-            pkgs.taglib
-            pkgs.openssl
-            pkgs.git
-            pkgs.libxml2
-            pkgs.libxslt
-            pkgs.libzip
-            pkgs.nix-direnv
-            pkgs.zlib
-
           ];
 
-          # shellHook = ''
-          # export CUDA_PATH=${pkgs.cudatoolkit}
-          # export EXTRA_CCFLAGS="-I/usr/include"
-          # export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
-          # '';
-
-          # Run this command, only after creating the virtual environment
-          postVenvCreation = ''
-            unset SOURCE_DATE_EPOCH
-          '';
-
-          # Now we can execute any commands within the virtual environment.
-          # This is optional and can be left out to run pip manually.
-          postShellHook = ''
-				# allow pip to install wheels
-				unset SOURCE_DATE_EPOCH
-
-          '';
         };
       }
     );
