d6fc7144802e62a6914f649190e32b108898a98e
Fabian Gubler <fabian.gubler@hotmail.com>
2023-12-13 03:47:56 +0100

    test formatting

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -2,6 +2,10 @@ import pytest  # pyright: ignore # noqa F401
 
 from utils.test_utils import process_and_merge_asts
 
+######################
+# Modification tests #
+######################
+
 
 def test_rule_based_modification():
     original_ast = "SUM(A2:A8)"
@@ -66,6 +70,11 @@ def test_function_outer_inner():
     assert user2_merged_ast_str == expected_output
 
 
+##########################
+# ADD FUNCTION ARGUMENTS #
+##########################
+
+
 def test_function_change_add_args():
     original_ast = "SUM(A2:A9)"
     user1_ast_str = "SUM(A2:A9, 5)"
@@ -110,100 +119,220 @@ def test_function_two_add_args():
     assert user2_merged_ast_str == expected_output
 
 
-# def test_function_nested_outer_inner():
-#     original_ast = "SUM(A1:A10)"
-#     user1_ast_str = "SUM(A1:A11)"
-#     user2_ast_str = "SUM(Average(A1:A10))"
+#############################
+# DELETE FUNCTION ARGUMENTS #
+#############################
+
+
+def test_integration_remove_argument_simple_function():
+    original_ast = "SUM(A1:A10, B1:B10)"
+    user1_ast_str = "SUM(A1:A10)"
+    user2_ast_str = "SUM(A1:A10, B1:B10, C1:C10)"
+
+    expected_output = "SUM(A1:A10, C1:C10)"
+
+    user1_merged_ast_str, user2_merged_ast_str = process_and_merge_asts(
+        original_ast,
+        user1_ast_str,
+        user2_ast_str,
+    )
+
+    assert user1_merged_ast_str == expected_output
+    assert user2_merged_ast_str == expected_output
+
+
+def test_integration_remove_argument_nested_function():
+    original_ast = "SUM(AVERAGE(A1:A10), B1)"
+    user1_ast_str = "SUM(AVERAGE(A1:A10))"
+    user2_ast_str = "SUM(AVERAGE(A1:A10), B1, C1)"
+
+    expected_output = "SUM(AVERAGE(A1:A10), C1)"
+
+    user1_merged_ast_str, user2_merged_ast_str = process_and_merge_asts(
+        original_ast,
+        user1_ast_str,
+        user2_ast_str,
+    )
+
+    assert user1_merged_ast_str == expected_output
+    assert user2_merged_ast_str == expected_output
+
+
+def test_integration_remove_multiple_arguments():
+    original_ast = "SUM(A1:A10, B1, C1:C10)"
+    user1_ast_str = "SUM(A1:A10)"
+    user2_ast_str = "SUM(A1:A10, D1:D10)"
+
+    expected_output = "SUM(A1:A10, D1:D10)"
+
+    user1_merged_ast_str, user2_merged_ast_str = process_and_merge_asts(
+        original_ast,
+        user1_ast_str,
+        user2_ast_str,
+    )
+
+    assert user1_merged_ast_str == expected_output
+    assert user2_merged_ast_str == expected_output
+
+
+#####################
+# CHANGE TYPE TESTS #
+#####################
+
+
+def test_change_cell_to_range_in_function():
+    original_ast_str = "SUM(A1)"
+    user1_ast_str = "SUM(A1)"
+    user2_ast_str = "SUM(A1:B1)"
+
+    expected_output = "SUM(A1:B1)"  # NOTE: if both cell-range then two arguments
+
+    user1_merged_ast_str, user2_merged_ast_str = process_and_merge_asts(
+        original_ast_str, user1_ast_str, user2_ast_str
+    )
+
+    assert user1_merged_ast_str == expected_output
+    assert user2_merged_ast_str == expected_output
+
+
+######################
+# ROOT LEVEL ADDITIONS
+######################
+
+
+# TODO: Key error parent
+
+# def test_add_outer_function_integration():
+#     original_ast_str = "A1"
+#     user1_ast_str = "SUM(A1)"
+#     user2_ast_str = "AVERAGE(A1)"
 #
-#     expected_output = "SUM(AVERAGE(A1:A10))"
+#     expected_output = "SUM(AVERAGE(A1))"
 #
 #     user1_merged_ast_str, user2_merged_ast_str = process_and_merge_asts(
-#         original_ast,
+#         original_ast_str,
 #         user1_ast_str,
-#         user2_ast_str,
-#         debug_changes=False,
-#         debug_new_asts=False,
-#         debug_merged_asts=False,
-#         debug_all=False,
+#         user2_ast_str
 #     )
 #
 #     assert user1_merged_ast_str == expected_output
 #     assert user2_merged_ast_str == expected_output
 
-# def test_conflicting_modification():
-#
-#     original_ast = "A1 + A2"
-#     user1_ast_str = "A1 + A3" # commited earlier (winner)
-#     user2_ast_str = "A1 + A4"
-#
-#     expected_output = "A1 + A3"
-#
-#     user1_merged_ast_str, user2_merged_ast_str = process_and_merge_asts(
-#         original_ast,
-#         user1_ast_str,
-#         user2_ast_str,
-#         debug_changes=False,
-#         debug_new_asts=False,
-#         debug_merged_asts=False,
-#         debug_all=True,
-#     )
-#
-#     assert user1_merged_ast_str == expected_output
-#     assert user2_merged_ast_str == expected_output
 
-# def test_apply_additions():
+# TODO: Key error parent
+
+# def test_unary_left_addition_integration():
+#     original_ast_str = "A1"
+#     user1_ast_str = "-A1"
+#     user2_ast_str = "+A1"
 #
-#     original_ast = "SUM(A1:A10)"
-#     user1_ast_str = "SUM(A1:A10, 5)"
-#     user2_ast_str = "SUM(A1:A10)"
-#
-#     expected_output = "SUM(A1:A10, 5)"
+#     expected_output = "-(+A1)"
 #
 #     user1_merged_ast_str, user2_merged_ast_str = process_and_merge_asts(
-#         original_ast,
-#         user1_ast_str,
-#         user2_ast_str,
-#         debug_changes=False,
-#         debug_new_asts=False,
-#         debug_merged_asts=False,
-#         debug_all=True,
+#         original_ast_str, user1_ast_str, user2_ast_str
 #     )
 #
 #     assert user1_merged_ast_str == expected_output
 #     assert user2_merged_ast_str == expected_output
 
 
-# def test_apply_deletions():
-#     original_ast = "SUM(A1:A10, 5)"
-#     user1_ast_str = "SUM(A1:A10, 5)"
-#     user2_ast_str = "SUM(A1:A10)"
+######################
+# ROOT LEVEL Deletions
+######################
+
+# TODO: Remove of type NONE
+
+# def test_delete_root_function_integration():
+#     original_ast_str = "SUM(A1)"
+#     user1_ast_str = "A1"
+#     user2_ast_str = "SUM(A1 * 2)"
+#
+#     expected_output = "A1 * 2"
+#
+#     user1_merged_ast_str, user2_merged_ast_str = process_and_merge_asts(
+#         original_ast_str,
+#         user1_ast_str,
+#         user2_ast_str
+#     )
+#
+#     assert user1_merged_ast_str == expected_output
+#     assert user2_merged_ast_str == expected_output
+
+
+
+
+#####################
+# ORDER MATTERS #
+#####################
+
+# TODO: Original node not found
+
+# def test_delete_root_binary_integration():
+#     original_ast_str = "SUM(A1:A10) + 1"
+#     user1_ast_str = "SUM(A1:A10)"
+#     user2_ast_str = "SUM(A1:A10) - 1"
+#
+#     expected_output = "SUM(A1:A10) - 1"
+#
+#     user1_merged_ast_str, user2_merged_ast_str = process_and_merge_asts(
+#         original_ast_str,
+#         user1_ast_str,
+#         user2_ast_str
+#     )
+#
+#     assert user1_merged_ast_str == expected_output
+#     assert user2_merged_ast_str == expected_output
+
+
+# TODO: First delete then add
+
+# def test_change_cell_to_number():
+#     original_ast_str = "A1 + 5"
+#     user1_ast_str = "10 + 5"
+#     user2_ast_str = "A1 + 10"
+#
+#     expected_output = "10 + 10"
+#
+#     user1_merged_ast_str, user2_merged_ast_str = process_and_merge_asts(
+#         original_ast_str, user1_ast_str, user2_ast_str
+#     )
+#
+#     assert user1_merged_ast_str == expected_output
+#     assert user2_merged_ast_str == expected_output
+
+# TODO: Binary already has two children
+
+# def test_binary_right_addition_integration():
+#     original_ast_str = "A1 + A2"
+#     user1_ast_str = "A1 + A2 + A3"
+#     user2_ast_str = "A1 + A2 - A4"
+#
+#     expected_output = "A1 + A2 + A3 - A4"
+#
+#     user1_merged_ast_str, user2_merged_ast_str = process_and_merge_asts(
+#         original_ast_str, user1_ast_str, user2_ast_str
+#     )
+#
+#     assert user1_merged_ast_str == expected_output
+#     assert user2_merged_ast_str == expected_output
+
+################
+# NEEDS FIXING #
+################
+
+
+# TODO: Cannot add to a deleted node
+
+# def test_root_type_modification():
+#     original_ast_str = "A1"
+#     user1_ast_str = "A1:A10"
+#     user2_ast_str = "SUM(A1)"
 #
 #     expected_output = "SUM(A1:A10)"
 #
 #     user1_merged_ast_str, user2_merged_ast_str = process_and_merge_asts(
-#         original_ast,
-#         user1_ast_str,
-#         user2_ast_str,
-#         debug_changes=False,
-#         debug_new_asts=False,
-#         debug_merged_asts=False,
+#         original_ast_str, user1_ast_str, user2_ast_str
 #     )
 #
 #     assert user1_merged_ast_str == expected_output
 #     assert user2_merged_ast_str == expected_output
-
-# def test_apply_complex_modifications():
-#     original_ast = parse("SUM(A1:A10, 5)")
-#     modified_ast = parse("SUM(A2:A9, 6)")
-#     changes = compare_asts(original_ast, modified_ast)
-#     new_ast = apply_changes_to_ast(original_ast, changes)
-#     assert str(new_ast) == "SUM(A2:A9, 6)"
-#
-#
-# def test_nested_modification():
-#     original_ast = parse("SUM(AVERAGE(A1:A5), A10)")
-#     modified_ast = parse("SUM(AVERAGE(A1:A6), A10)")
-#     changes = compare_asts(original_ast, modified_ast)
-#     new_ast = apply_changes_to_ast(original_ast, changes)
-#     assert str(new_ast) == "SUM(AVERAGE(A1:A6), A10)"
-#
--- tests/unit/test_apply_changes.py
+++ tests/unit/test_apply_changes.py
@@ -94,14 +94,6 @@ def test_mixed_modifications_and_structural_changes():
     assert str(new_ast) == "SUM(A2:A9, MAX(B2:B6))"
 
 
-def test_root_type_modification():
-    original_ast = parse("A1")
-    modified_ast = parse("A1:A10")
-    changes = compare_asts(original_ast, modified_ast)
-    new_ast, _ = apply_changes_to_ast(original_ast, changes, user_id="test")
-    assert str(new_ast) == "A1:A10"
-
-
 ##########################
 # ADD FUNCTION ARGUMENTS #
 ##########################
@@ -229,6 +221,14 @@ def test_change_cell_to_number():
     assert str(new_ast) == "10 + 5"
 
 
+def test_root_type_modification():
+    original_ast = parse("A1")
+    modified_ast = parse("A1:A10")
+    changes = compare_asts(original_ast, modified_ast)
+    new_ast, _ = apply_changes_to_ast(original_ast, changes, user_id="test")
+    assert str(new_ast) == "A1:A10"
+
+
 ######################
 # ROOT LEVEL ADDITIONS
 ######################
@@ -324,22 +324,6 @@ def test_delete_root_binary():
 #     new_ast, _ = apply_changes_to_ast(original_ast, changes, user_id="test")
 #     assert str(new_ast) == "A3 + A1 + A2"
 
-
-##############################
-# FUNCTION ARGUMENTS + TYPES #
-##############################
-
-# def test_function_if_modification():
-#     original_ast = parse("IF(A1, HIGH, LOW)")
-#     modified_ast = parse("IF(A1, HIGH, IF(A1, MEDIUM, LOW))")
-#     changes = compare_asts(original_ast, modified_ast)
-#     new_ast, _ = apply_changes_to_ast(original_ast, changes, user_id="test")
-#     assert str(new_ast) == "IF(A1, HIGH, IF(A1, MEDIUM, LOW))"
-
-
-#####################
-# Arbitrary changes #
-#####################
 
 # def test_complex_structural_change():
 #     original_ast = parse("SUM(A1:A10)")
