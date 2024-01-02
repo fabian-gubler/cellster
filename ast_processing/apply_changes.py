9f9b2a0fc74c25dcc3aba73a2fc8367be54eb2db
Fabian Gubler <fabian.gubler@hotmail.com>
2024-01-02 12:16:01 +0100

    remove updated_nodes for now

--- ast_processing/apply_changes.py
+++ ast_processing/apply_changes.py
@@ -21,33 +21,29 @@ class StructuralChangeException(Exception):
 
 
 def apply_changes_to_ast(original_ast, changes, user_id):
-    updated_nodes: list = []
     for change in changes:
         match change:
             case NodeModification():
                 # node_to_modify = find_node(original_ast, change["original"].id_history)
-
-                updated_node = modify_node(change, user_id)
+                modify_node(change, user_id)
 
             case ChildAddition():
                 # parent_node = find_node(original_ast, change["parent"].id_history)
-                updated_node = add_child(change, user_id)
+                add_child(change, user_id)
 
             case ChildDeletion():
                 # parent_node = find_node(original_ast, change["parent"].id_history)
-                updated_node = remove_child(change, user_id)
+                remove_child(change, user_id)
 
             case RootAddition():
                 # child_node = find_node(original_ast, change["child"].id_history)
-                original_ast, updated_node = add_root(original_ast, change, user_id)
+                add_root(original_ast, change, user_id)
 
             case RootDeletion():
                 # new_root_node = find_node(original_ast, change["child"].id_history)
-                original_ast, updated_node = remove_root(original_ast, change, user_id)
+                remove_root(original_ast, change, user_id)
 
             case _:
                 raise StructuralChangeException("Change type not found")
 
-        updated_nodes.append(updated_node)
-
-    return original_ast, updated_nodes
+    return original_ast
