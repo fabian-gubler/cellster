d00287e6c9b1db54852c300c3f8971b304c69748
Fabian Gubler <fabian.gubler@hotmail.com>
2024-01-25 02:28:10 +0100

    refactor: add base clase for type hinting

--- ast_utils/change_classes.py
+++ ast_utils/change_classes.py
@@ -1,45 +1,51 @@
-from typing import Union
 from parser.nodes import BaseNode
+from typing import Union
 
+# Python type hinting shenanigans
+class Change:
+    def __init__(self):
+        pass
 
-class NodeModification:
+class NodeModification(Change):
     def __init__(self, original_node: BaseNode, new_node: BaseNode):
         self.original_node = original_node
         self.new_node = new_node
 
 
-class ChildAddition:
+class ChildAddition(Change):
     def __init__(self, parent_node: BaseNode, child_node: BaseNode):
         self.parent_node = parent_node
         self.child_node = child_node
 
 
-class ChildDeletion:
+class ChildDeletion(Change):
     def __init__(self, parent_node: BaseNode, child_node: BaseNode):
         self.parent_node = parent_node
         self.child_node = child_node
 
 
-class RootAddition:
-    def __init__(self, parent_node: BaseNode, child_node: BaseNode, direction: Union[None, str]):
+class RootAddition(Change):
+    def __init__(
+        self, parent_node: BaseNode, child_node: BaseNode, direction: Union[None, str]
+    ):
         self.parent_node = parent_node
         self.child_node = child_node
         self.direction = direction
 
 
-class RootDeletion:
+class RootDeletion(Change):
     def __init__(self, parent_node: BaseNode, child_node: BaseNode):
         self.parent_node = parent_node
         self.child_node = child_node
 
 
-class RootNodeModification:
+class RootNodeModification(Change):
     def __init__(self, original_root_node: BaseNode, new_root_node: BaseNode):
         self.original_root_node = original_root_node
         self.new_root_node = new_root_node
 
 
-class StructuralChange:
+class StructuralChange(Change):
     def __init__(self, original_node: BaseNode, new_node: BaseNode):
         self.original_node = original_node
         self.new_node = new_node
